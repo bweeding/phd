b1a138acab759be5a587e8fa1b987f907b4595ed
bweeding <57697604+bweeding@users.noreply.github.com>
2020-10-29 10:59:32 +1100

    Update metfile_compiler.py
    modified to allow barra_nc_to_df to take file paths instead of names, began building merger

--- metfile_compiler.py
+++ metfile_compiler.py
@@ -15,30 +15,55 @@ import pandas as pd
 os.chdir('C:\\Users\\weedingb\\Documents\\GitHub\\phd')
 
 # set wdir to netcdf location
-os.chdir('C:\\Users\\weedingb\\Desktop\\barra_temp_sample')
+os.chdir('C:/Users/weedingb/Desktop/barra_temp_sample')
+
+# set wdir to netcdf location
+os.chdir('C:/Users/weedingb/Desktop/barra dir test')
+
+# for root, dirs, files in os.walk('C:\\Users\\weedingb\\Desktop\\Barra dir test'):
+    
+#     print(files)
+    
+total_df = pd.DataFrame()
+
+for root, dirs, files in os.walk(r'C:/Users/weedingb/Desktop/Barra dir test'):
+    
+    for fname in files:
+    
+        print(os.path.join(root,fname))
+        
+        file_loc = os.path.join(root,fname)
+        
+        nc = barra_nc_to_df(file_loc.replace(os.sep, '/'))
+        
+        nc.close()
+    
+        #total_df.merge(barra_nc_to_df(os.path.join(root,fname)))
+
 
 # =============================================================================
 # Extracts the date/time information and main variable from a Barra netcdf. 
 # Default lat/lon are for Franklin Square in Hobart.
 # =============================================================================
 
-def barra_nc_to_df(nc_filename, target_lat = -42.882808, target_lon = 147.330266):
+def barra_nc_to_df(nc_file_loc, target_lat = -42.882808, target_lon = 147.330266):
     
-    # opens the netcdf specified by the filename
-    nc = Dataset(nc_filename,mode='r')
+    # opens the netcdf specified by the file location
+    nc = Dataset(nc_file_loc,mode='r')
 
     # sets the string that signifies the end of the main variable in the filename format
     splitter = '-fc'
     
     # extracts the main variable name for the file from the filename
-    nc_main_varname = nc_filename.rpartition(splitter)[0]
+    nc_main_varname = nc_file_loc.rpartition(splitter)[0]
+    nc_main_varname = nc_file_loc.rpartition('/')[-1]
     
     # find indices of closest lat and lon in netcdf
     lat_idx = (np.abs(np.array(nc.variables['latitude'][:])-target_lat)).argmin()
     lon_idx = (np.abs(np.array(nc.variables['longitude'][:])-target_lon)).argmin()
     
     # extracts main variable from netcdf at closest lat/lon for all timestamps
-    nc_main = np.array(nc.variables[nc_main_varname][:,target_lat_idx,target_lon_idx])
+    nc_main = np.array(nc.variables[nc_main_varname][:,lat_idx,lon_idx])
     
     # extracts time data (in hours since 01/01/1970 00:00:00)
     nc_time = np.array(nc.variables['time'][:])
@@ -63,42 +88,42 @@ def barra_nc_to_df(nc_filename, target_lat = -42.882808, target_lon = 147.330266
 
 
 
-nc = Dataset('temp_scrn-fc-spec-PT1H-utas-v1.2-20150101T0000Z.nc',mode='r')
+# nc = Dataset('temp_scrn-fc-spec-PT1H-utas-v1.2-20150101T0000Z.nc',mode='r')
 
-# Use ~ Franklin square lat lon: -42.882808      147.330266 
-target_lat = -42.882808
-target_lon = 147.330266 
+# # Use ~ Franklin square lat lon: -42.882808      147.330266 
+# target_lat = -42.882808
+# target_lon = 147.330266 
 
-# find index 
-nc_lat = np.array(nc.variables['latitude'][:])
-nc_lon = np.array(nc.variables['longitude'][:])
+# # find index 
+# nc_lat = np.array(nc.variables['latitude'][:])
+# nc_lon = np.array(nc.variables['longitude'][:])
 
-target_lat_idx = (np.abs(nc_lat-target_lat)).argmin()
-target_lon_idx = (np.abs(nc_lon-target_lon)).argmin()
+# target_lat_idx = (np.abs(nc_lat-target_lat)).argmin()
+# target_lon_idx = (np.abs(nc_lon-target_lon)).argmin()
 
-target_nc_temp_scrn = np.array(nc.variables['temp_scrn'][:,target_lat_idx,target_lon_idx])
+# target_nc_temp_scrn = np.array(nc.variables['temp_scrn'][:,target_lat_idx,target_lon_idx])
 
-target_nc_time = np.array(nc.variables['time'][:])
+# target_nc_time = np.array(nc.variables['time'][:])
 
-pd.to_datetime(target_nc_time,unit='h')
+# pd.to_datetime(target_nc_time,unit='h')
 
-target_nc_data = list(zip(pd.to_datetime(target_nc_time,unit='h').year,pd.to_datetime(target_nc_time,unit='h').dayofyear,pd.to_datetime(target_nc_time,unit='h').hour,target_nc_temp_scrn))
+# target_nc_data = list(zip(pd.to_datetime(target_nc_time,unit='h').year,pd.to_datetime(target_nc_time,unit='h').dayofyear,pd.to_datetime(target_nc_time,unit='h').hour,target_nc_temp_scrn))
 
-target_nc_df = pd.DataFrame(target_nc_data,columns=['Year','DOY','Hour','Scrn T'])
+# target_nc_df = pd.DataFrame(target_nc_data,columns=['Year','DOY','Hour','Scrn T'])
 
 
-['longitude',
- 'time',
- 'latitude',
- 'height',
- 'latitude_longitude',
- 'forecast_period',
- 'forecast_reference_time',
- 'temp_scrn']
+# ['longitude',
+#  'time',
+#  'latitude',
+#  'height',
+#  'latitude_longitude',
+#  'forecast_period',
+#  'forecast_reference_time',
+#  'temp_scrn']
 
-#units: hours since 1970-01-01 00:00:00
+# #units: hours since 1970-01-01 00:00:00
 
-met_df = pd.DataFrame(columns=['%iy','id','it','imin','Q*','QH','QE','Qs','Qf','Wind','RH','Td','press','rain','Kdn','snow','ldown','fcld','wuh','xsmd','lai_hr','Kdiff','Kdir','Wd'])
+# met_df = pd.DataFrame(columns=['%iy','id','it','imin','Q*','QH','QE','Qs','Qf','Wind','RH','Td','press','rain','Kdn','snow','ldown','fcld','wuh','xsmd','lai_hr','Kdiff','Kdir','Wd'])
 
 
 
